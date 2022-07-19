2882c01d029f5e29ea5a7c81fbbf8a87a73241b6
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-07-18 21:56:29 -0300

    commit

--- src/store/layout/actions.test.js
+++ src/store/layout/actions.test.js
@@ -1,27 +1,27 @@
 import {
-    Types,
-    Creators
+    hideMessage,
+    showMessage
 } from "./index";
 
 describe('Layout actions', () => {
     describe('Actions Types', () => {
         test('Deve retornar a action type SHOW_MESSAGE', () => {
-            expect(Types.SHOW_MESSAGE).toBe('SHOW_MESSAGE');
+            expect(showMessage.type).toBe('SHOW_MESSAGE');
         });
         test('Deve retornar a action type HIDE_MESSAGE', () => {
-            expect(Types.HIDE_MESSAGE).toBe('HIDE_MESSAGE');
+            expect(hideMessage.type).toBe('HIDE_MESSAGE');
         });
     });
 
     describe('Actions Creators', () => {
         test('Deve retornar a action creator showMessage', () => {
-            expect(Creators.showMessage()).toEqual({
-                type: Types.SHOW_MESSAGE
+            expect(showMessage()).toEqual({
+                type: showMessage.type
             });
         });
         test('Deve retornar a action creator hideMessage', () => {
-            expect(Creators.hideMessage()).toEqual({
-                type: Types.HIDE_MESSAGE
+            expect(hideMessage()).toEqual({
+                type: hideMessage.type
             });
         });
     })
